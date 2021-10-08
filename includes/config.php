f2920537cdeef5250bb827c6895342f38d0d7a1a
ayazhan <shansharovaayazhan@gmail.com>
2021-10-08 11:48:37 +0600

    first commit

---
+++ includes/config.php
@@ -0,0 +1,14 @@
+<?php
+$config = array(
+    'title'=>'my blog',
+    'vk-url'=> "http://vk.com/somebody",
+    'db'=> array(
+        'server'=>'localhost',
+        'username'=>'mysql',
+        'password'=>'',
+        'name'=>'blog'
+    )
+
+);
+
+require 'db.php';
